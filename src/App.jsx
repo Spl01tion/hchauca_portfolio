970a435c7ddb45e6c0daf3bb129c0e3c87df670f
Hericles Chauca <69146181+Spl01tion@users.noreply.github.com>
2025-12-15 09:25:54 +0200

    fix: update resume name and replace Skills component with Projects in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
-import Skills from "./components/sections/Skills";
+import Projects from "./components/sections/Projects";
 
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
     <main>
       <Hero />
       <About />
-      <Skills/>
+      <Projects/>
     </main>
     </div>
   )
